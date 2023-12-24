59fc44860cc73bde6755ae80d2cfe6a04d4ae660
Stephanie Ondo <98669815+sloart@users.noreply.github.com>
2023-12-24 15:58:54 -0500

    started this project today
    an image quilt of all the books i've read.

---
+++ all-the-books/scripts.js
@@ -0,0 +1,22 @@
+
+
+// When the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all images with the class 'book-cover'
+    var covers = document.querySelectorAll('.book-cover');
+    
+    // Add event listeners to each cover
+    covers.forEach(function(cover) {
+        cover.addEventListener('mouseover', function() { bigImg(this); });
+        cover.addEventListener('mouseout', function() { normalImg(this); });
+    });
+});
+
+function bigImg(x) {
+  x.style.transform = "scale(1.1,1.1)";
+
+}
+
+function normalImg(x) {
+  x.style.transform = "scale(1,1)";
+}
